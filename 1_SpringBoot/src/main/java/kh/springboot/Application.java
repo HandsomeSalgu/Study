70a69018d530765a5ae7e098c9937d87b4ac9898
KH-taehyuk <warsfac@gmail.com>
2025-01-03 09:02:06 +0900

    250103 수정완료

--- 1_SpringBoot/src/main/java/kh/springboot/Application.java
+++ 1_SpringBoot/src/main/java/kh/springboot/Application.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // 이 클래스가 Main클래스임을 Spring boot에게 알려주는 어노테이션
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
